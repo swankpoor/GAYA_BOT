372f1f6cdfe91758a800d7b69dadfb0966404def
Swankpoor <swankpoor@gmail.com>
2025-11-27 23:38:48 -0300

    Create intelligent_responses.py

---
+++ intelligent_responses.py
@@ -0,0 +1,6 @@
+# intelligent_responses.py
+from database_manager import contar_transportes, verificar_chassis_repetidos
+
+def interpretar_pergunta(pergunta: str, db_path: str = 'transportes.db'):
+    # Sistema de respostas amigáveis
+    pass
